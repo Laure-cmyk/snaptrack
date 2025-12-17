01925fcc9977c6fa4104f9be1f5660acc0f39cb4
B-Nikola <nikola.bouattit@gmail.com>
2025-12-17 16:13:20 +0100

    try to fix the upload on render

--- back/app.js
+++ back/app.js
@@ -50,7 +50,7 @@ app.use('/scores', scoresRouter);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
   
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 }
