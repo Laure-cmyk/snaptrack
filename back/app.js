ddb33287f083059a47f7f254a85534eecd76c874
B-Nikola <nikola.bouattit@gmail.com>
2025-12-18 22:39:52 +0100

    Fixed path

--- back/app.js
+++ back/app.js
@@ -50,7 +50,7 @@ app.use('/scores', scoresRouter);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
   
-  app.get('/{*path}', (req, res) => {
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 }
