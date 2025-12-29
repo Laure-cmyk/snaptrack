ead25d3bcd01f043799a6100cbbdab54798b3b3a
Helder <helder.lopesvarela@heig-vd.ch>
2025-12-29 13:31:58 +0100

    add Header component, update comments and rename parcours to trail

--- front/components/create/CreateList.vue
+++ front/components/create/CreateList.vue
@@ -2,7 +2,7 @@
 import BaseCard from '@/components/BaseCard.vue'
 
 defineProps({
-    parcours: {
+    trails: {
         type: Array,
         required: true
     }
@@ -14,7 +14,7 @@ defineEmits(['create-new', 'edit', 'delete'])
 <template>
     <div class="pa-6">
         <!-- Empty State -->
-        <div v-if="parcours.length === 0" class="text-center py-12">
+        <div v-if="trails.length === 0" class="text-center py-12">
             <v-icon size="80" color="grey-lighten-1">mdi-map-marker-path</v-icon>
             <div class="text-h6 text-grey mt-4 mb-2">Aucun parcours créé</div>
             <div class="text-body-2 text-grey mb-6">Commencez par créer votre premier parcours</div>
@@ -24,11 +24,11 @@ defineEmits(['create-new', 'edit', 'delete'])
             </v-btn>
         </div>
 
-        <!-- Parcours List -->
+        <!-- Trails List -->
         <div v-else>
-            <!-- Parcours Cards -->
+            <!-- Trail Cards -->
             <v-row>
-                <v-col cols="12" v-for="item in parcours" :key="item.id">
+                <v-col cols="12" v-for="item in trails" :key="item.id">
                     <BaseCard :title="item.title" :description="item.description" :image="item.image" :city="item.city"
                         :show-delete="true" :clickable="true" @click="$emit('edit', item)"
                         @delete="$emit('delete', item)" />
@@ -37,7 +37,7 @@ defineEmits(['create-new', 'edit', 'delete'])
         </div>
 
         <!-- Floating Action Button -->
-        <v-btn v-if="parcours.length > 0" icon color="indigo-darken-1" size="x-large" class="fab-button" elevation="6"
+        <v-btn v-if="trails.length > 0" icon color="indigo-darken-1" size="x-large" class="fab-button" elevation="6"
             @click="$emit('create-new')">
             <v-icon color="white" size="large">mdi-plus</v-icon>
         </v-btn>
