7fe4e3fece91e585cf40deae90e8e2dd087a7893
B-Nikola <nikola.bouattit@gmail.com>
2026-01-05 13:51:24 +0100

    Ajout de la virgule
    Manquait une virgule ,

--- front/app.js
+++ front/app.js
@@ -20,7 +20,7 @@ const routes = [
   { path: '/friendlist', component: () => import('./pages/PageFriendlist.vue'), meta: { requiresAuth: true } },
   { path: '/create-challenge', component: () => import('./pages/PageCreateChallenge.vue'), meta: { requiresAuth: true } },
   { path: '/profil', component: () => import('./pages/PageProfile.vue'), meta: { requiresAuth: true } },
-  { path: '/authentification', component: () => import('./pages/PageAuth.vue') }
+  { path: '/authentification', component: () => import('./pages/PageAuth.vue') },
   { path: '/socials', component: () => import('./pages/PageSocials.vue') }
 ];
 
